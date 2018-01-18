8cf3b72f129961a70d221adaec676e42d3cdc06c
Chris Hartgerink <chjh@protonmail.com>
2018-01-18 15:00:05 +0100

    Make script run sync
    Async was causing trouble in writing out. I don't yet fully understand how objects transfer between async and sync, it seems

--- scripts/associated-topics.js
+++ scripts/associated-topics.js
@@ -1,48 +1,40 @@
 #!/bin/node
 
 const fs = require('fs')
-var outFile = 'assets/graphFile.json'
-var outObj = {"nodes": [], "links": []}
+let outFile = 'assets/graphFile.json'
+let outObj = { nodes: [], links: [] }
+
 
 function findTopics (dir, out) {
-  fs.readdir(dir, function (err, files) {
-    if (err) {
-      console.log(err)
-    }
+    let files = fs.readdirSync(dir)
 
-    files.forEach( function (file) {
-      pushTopics(file, out)
+    files.forEach((file) => {
+      out.nodes.push({ name: file, group: 1})
       findRefs(file, out)
     })
-  })
-}
-
-function pushTopics (file, out) {
-  out.nodes.push({"name": file, "group": 1})
-}
+	  console.log(out)
+  }
 
 function findRefs (file, out) {
-  fs.readFile(`content/${file}`, function (err, contents) {
-    if (err) {
-      return console.log(err)
-    }
+    const contents =  fs.readFileSync(`content/${file}`)
     const txt = contents.toString()
     const refs = txt.match(/(\w-?)+.md/g)
     const toLink = coArray(refs, arrayTopics(out))
     const source = file.replace('content/', '')
-    console.log(source)
-    toLink.forEach( function (link) {
-      pushLinks(source, link, 1, out)
-    })
-  })
-}
+    toLink.forEach((link) => {
+//	    console.log(source)
+//	    console.log(link)
+//	    console.log(out)
+        pushLinks(source, link, 1, out)
+      })
+  }
 
 // https://stackoverflow.com/questions/12433604/how-can-i-find-matching-values-in-two-arrays#12433654
 function coArray (array1, array2) {
   var ret = []
   array1.sort()
   array2.sort()
-  for(var i = 0; i < array1.length; i += 1) {
+  for(let i = 0; i < array1.length; i += 1) {
       if(array2.indexOf( array1[i] ) > -1){
           ret.push(array1[i])
       }
@@ -51,9 +43,9 @@ function coArray (array1, array2) {
 }
 
 function pushLinks (source, target, weight, out) {
-  out.links.push({"source": indexTopic(source, out),
-  "target": indexTopic(target, out),
-  "weight": weight})
+  out.links.push({ source: indexTopic(source, out),
+    target: indexTopic(target, out),
+    weight: weight})
 }
 
 function indexTopic (topic, out) {
@@ -61,14 +53,13 @@ function indexTopic (topic, out) {
 }
 
 function arrayTopics (out) {
-  return out.nodes.map(function (value) {return value['name']})
+  return out.nodes.map((value) => {return value['name']})
 }
 
 findTopics('content/', outObj)
 
+console.log(outObj)
 // Write out JSON object
-fs.writeFile(outFile, JSON.stringify(outObj), function(err) {
-    if(err) {
-        return console.log(err);
-    }
+fs.writeFile(outFile, JSON.stringify(outObj), (err) => {
+    if(err) throw err
 })
