19212110b034b9f0852329ee36c5e27401439a4d
Chris Hartgerink <chjh@protonmail.com>
2018-02-08 13:21:00 +0100

    Update generate-pages.sh

--- scripts/generate-pages.sh
+++ scripts/generate-pages.sh
@@ -2,7 +2,7 @@
 
 for FILE in `ls content/*.md`;
 do
-  cat fork-template.html | sed "s#REPLACE_ME#$FILE#g" > tmp.html
+  cat assets/fork-template.html | sed "s#REPLACE_ME#$FILE#g" > tmp.html
   pandoc -f markdown -t html $FILE -o $FILE.html --css=assets/styles.css -B tmp.html
   rm tmp.html
 done
