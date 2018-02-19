bcf1f7beab67174e03226b0d218cc425a31d2038
Chris Hartgerink <chjh@protonmail.com>
2018-02-19 17:39:22 +0100

    Correct CSS path

--- scripts/generate-pages.sh
+++ scripts/generate-pages.sh
@@ -3,6 +3,6 @@
 for FILE in `ls content/*.md`;
 do
   cat assets/fork-template.html | sed "s#REPLACE_ME#$FILE#g" > tmp.html
-  pandoc -f markdown -t html $FILE -o $FILE.html --css=assets/styles.css -B tmp.html
+  pandoc -f markdown -t html $FILE -o $FILE.html --css=../assets/styles.css -B tmp.html
   rm tmp.html
 done
